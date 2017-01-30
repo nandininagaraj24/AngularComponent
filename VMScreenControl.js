bb31d86b80ab4a360d0d821946889b4a25a7026c
Nandini Nagaraj <nandini.nagaraj24@gmail.com>
2017-01-29 20:51:32 -0500

    Update VMScreenControl.js

--- VMScreenControl.js
+++ VMScreenControl.js
@@ -4,14 +4,18 @@ var vmScreenModule = angular.module('vmScreenModule', []);
 vmScreenModule.directive('vmScreen', function () {
     return {
 		
-        restrict: 'EA',
+        restrict: 'E',
 		scope: {},
         templateUrl: "VMScreen.html",
         controller: ['$scope', function ($scope) {
                 
                 
                 $scope.status = "ERROR";
-                $scope.name="Hello";
+                $scope.name="Critical VMs";
+		$scope.description = "All my business-critical VMs.";
+		$scope.path = "/TINTRI/CRITICAL-VMs";
+		$scope.numOfVMs = "16";
+		$scope.numOfErrors = "2";
            
         }]
     };
